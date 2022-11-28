0104e05baf967e00f757fe0c7857d38652d617ec
고동우 <ehddn2202@gmail.com>
2022-11-28 16:52:50 +0900

    chapter 8-3-2

--- src/main/java/com/example/account/domain/Account.java
+++ src/main/java/com/example/account/domain/Account.java
@@ -1,8 +1,8 @@
 package com.example.account.domain;
 
+import com.example.account.type.AccountStatus;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
