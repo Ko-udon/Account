f4915e87a128b0f4951a96df75d278c2d1928551
Kodongwoo <ehddn2202@naver.com>
2022-11-29 00:18:51 +0900

    temp

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -4,15 +4,23 @@ import com.example.account.domain.Account;
 import com.example.account.service.AccountService;
 import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
     private final RedisTestService redisTestService;
+
+    @PostMapping("/account")
+    public String createAccount(
+            @RequestBody @Valid CreateAccount
+            ){
+        accountService.createAccount();
+        return "success";
+    }
 
     @GetMapping("/get-lock")
     public String getLock(){
